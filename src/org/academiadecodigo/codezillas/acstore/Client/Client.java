f9a24ddc05ec017bfda5ae12520ad1cd8fb4d924
Jess <jess.c.nunes@gmail.com>
2019-07-13 22:29:30 +0000

    client menu added

--- src/org/academiadecodigo/codezillas/acstore/Client/Client.java
+++ src/org/academiadecodigo/codezillas/acstore/Client/Client.java
@@ -1,7 +1,52 @@
 package org.academiadecodigo.codezillas.acstore.Client;
 
+import org.academiadecodigo.bootcamp.Prompt;
+import org.academiadecodigo.bootcamp.scanners.menu.MenuInputScanner;
+import org.academiadecodigo.codezillas.acstore.Server.Server;
+
 public class Client {
-    public static void main(String[] args) {
+
+    private Prompt prompt;
+    private Server server;
+
+    public Client() {
+        this.server = server;
+    }
+
+    public void menu() {
+
+        prompt = new Prompt(System.in, System.out);
+
+        String[] menuArr = {"Beer", "Coffee", "Water"};
+        MenuInputScanner menuOptions = new MenuInputScanner(menuArr);
+
+        int numOfOption = prompt.getUserInput(menuOptions);
+
+        int askedBeerNumber = prompt.getUserInput(server.consumedBeer);
+
+        int askedCoffeeNumber = prompt.getUserInput(server.consumedCoffee);
+
+        int askedWaterNumber = prompt.getUserInput(server.consumedWater);
+
+
+        switch (numOfOption) {
+
+            case 1:
+                System.out.println("Here's your beer! Enjoy.");
+                server.consumedBeer(prompt.getUserInput(askedBeerNumber));
+                break;
+
+            case 2:
+                System.out.println("Here's your coffee, enjoy!");
+                server.consuemdCoffee(prompt.getUserInput(askedCoffeeNumber));
+                break;
+
+            case 3:
+                System.out.println("Here's your healthy water, enjoy!");
+                server.consumedWater(prompt.getUserInput(askedWaterNumber));
+                break;
+        }
 
     }
+
 }
---
+++ src/org/academiadecodigo/codezillas/acstore/Client/ClientLauncher.java
@@ -0,0 +1,8 @@
+package org.academiadecodigo.codezillas.acstore.Client;
+
+public class ClientLauncher {
+
+    public static void main(String[] args) {
+
+    }
+}
